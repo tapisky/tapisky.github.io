56a839a762dea7b5407348ebf7f005263effc5af
tapisky <jctapiador@gmail.com>
2014-06-24 18:38:05 +0200

    Adding pages

---
+++ js/jslib.js
@@ -0,0 +1,344 @@
+
+// Gets the browser prefix
+var browserPrefix;
+navigator.sayswho= (function(){
+  var N = navigator.appName, ua = navigator.userAgent, tem;
+  var M = ua.match(/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i);
+  if(M && (tem = ua.match(/version\/([\.\d]+)/i))!= null) M[2] = tem[1];
+  M = M? [M[1], M[2]]: [N, navigator.appVersion,'-?'];
+  M = M[0];
+  if(M == "Chrome") { browserPrefix = "webkit"; }
+  if(M == "Firefox") { browserPrefix = "moz"; }
+  if(M == "Safari") { browserPrefix = "webkit"; }
+  if(M == "MSIE") { browserPrefix = "ms"; }
+})();
+
+function switch_language(language)
+{
+  switch(language)
+  {
+    case 'german':
+      elm = document.getElementById('about_link');
+      elm.innerHTML = german_link_about;
+      elm = document.getElementById('calendar_link');
+      elm.innerHTML = german_link_calendar;
+      elm = document.getElementById('repertoire_link');
+      elm.innerHTML = german_link_repertoire;
+      elm = document.getElementById('multimedia_link');
+      elm.innerHTML = german_link_multimedia;
+      elm = document.getElementById('gallery_link');
+      elm.innerHTML = german_link_gallery;
+      elm = document.getElementById('contact_link');
+      elm.innerHTML = german_link_contact;
+      elm = document.getElementById('info');
+      elm.innerHTML = german_frontpage;
+      elm = document.getElementById('about_link');
+      elm.innerHTML = german_link_about;
+      elm = document.getElementById('about-text');
+      elm.innerHTML = german_about;
+      elm = document.getElementById('calendar-text');
+      elm.innerHTML = german_calendar;
+      elm = document.getElementById('repertoire-text');
+      elm.innerHTML = german_repertoire;
+      // elm = document.getElementById('gallery-text');
+      // elm.innerHTML = german_gallery;
+      elm = document.getElementById('contact-text');
+      elm.innerHTML = german_contact;
+      break;
+
+    case 'english':
+      elm = document.getElementById('about_link');
+      elm.innerHTML = english_link_about;
+      elm = document.getElementById('calendar_link');
+      elm.innerHTML = english_link_calendar;
+      elm = document.getElementById('repertoire_link');
+      elm.innerHTML = english_link_repertoire;
+      elm = document.getElementById('multimedia_link');
+      elm.innerHTML = english_link_multimedia;
+      elm = document.getElementById('gallery_link');
+      elm.innerHTML = english_link_gallery;
+      elm = document.getElementById('contact_link');
+      elm.innerHTML = english_link_contact;
+      elm = document.getElementById('info');
+      elm.innerHTML = english_frontpage;
+      elm = document.getElementById('about_link');
+      elm.innerHTML = english_link_about;
+      elm = document.getElementById('about-text');
+      elm.innerHTML = english_about;
+      elm = document.getElementById('calendar-text');
+      elm.innerHTML = english_calendar;
+      elm = document.getElementById('repertoire-text');
+      elm.innerHTML = english_repertoire;
+      // elm = document.getElementById('gallery-text');
+      // elm.innerHTML = english_gallery;
+      elm = document.getElementById('contact-text');
+      elm.innerHTML = english_contact;
+      break;
+
+    case 'polish':
+      elm = document.getElementById('about_link');
+      elm.innerHTML = polish_link_about;
+      elm = document.getElementById('calendar_link');
+      elm.innerHTML = polish_link_calendar;
+      elm = document.getElementById('repertoire_link');
+      elm.innerHTML = polish_link_repertoire;
+      elm = document.getElementById('multimedia_link');
+      elm.innerHTML = polish_link_multimedia;
+      elm = document.getElementById('gallery_link');
+      elm.innerHTML = polish_link_gallery;
+      elm = document.getElementById('contact_link');
+      elm.innerHTML = polish_link_contact;
+      elm = document.getElementById('info');
+      elm.innerHTML = polish_frontpage;
+      elm = document.getElementById('about_link');
+      elm.innerHTML = polish_link_about;
+      elm = document.getElementById('about-text');
+      elm.innerHTML = polish_about;
+      elm = document.getElementById('calendar-text');
+      elm.innerHTML = polish_calendar;
+      elm = document.getElementById('repertoire-text');
+      elm.innerHTML = polish_repertoire;
+      // elm = document.getElementById('gallery-text');
+      // elm.innerHTML = polish_gallery;
+      elm = document.getElementById('contact-text');
+      elm.innerHTML = polish_contact;
+      break;
+    default:
+
+  }
+}
+
+// JQuery functions
+$( document ).ready(function() {
+
+  // $('#main-container').css('margin-left','-' + String(screen.width/2) + 'px');
+  $('#main-container').css('margin-left','-640px');
+
+
+  $('.container,.container-opac').fadeOut(1);
+  $('#loader').fadeOut(2500);
+
+  $('#home_link').click(function(){
+    $('.container,.container-opac').fadeOut('slow');
+  });
+
+  $('#about_link').click(function(){
+    $('.container,.container-opac').fadeOut('slow');
+    setTimeout(function(){
+      // $('#about-text').scrollTop(0);
+      $('#about-content').fadeIn('slow');
+    }, 250);
+  });
+
+  $('#calendar_link').click(function(){
+    $('.container,.container-opac').fadeOut('slow');
+    setTimeout(function(){
+      $('#calendar-content').fadeIn('slow');
+    }, 250);
+  });
+
+  $('#repertoire_link').click(function(){
+    $('.container,.container-opac').fadeOut('slow');
+    setTimeout(function(){
+      $('#repertoire-content').fadeIn('slow');
+    }, 250);
+  });
+
+  $('#multimedia_link').click(function(){
+    $('.container,.container-opac').fadeOut('slow');
+    setTimeout(function(){
+      $('#multimedia-content').fadeIn('slow');
+    }, 250);
+  });
+
+  $('#gallery_link').click(function(){
+    $('.container,.container-opac').fadeOut('slow');
+    setTimeout(function(){
+      $('#gallery-content').fadeIn('slow');
+    }, 250);
+  });
+
+  $('#contact_link').click(function(){
+    $('.container,.container-opac').fadeOut('slow');
+    setTimeout(function(){
+      $('#contact-content').fadeIn('slow');
+    }, 250);
+  });
+
+  //
+  // Gallery code
+  //
+
+  $('#left-arrow').fadeOut();
+
+  imgarray = new Array();
+  i = 0;
+
+  $('.picviewer').each(function(){
+    if (i != 0)
+    {
+      $(this).fadeOut();
+    }
+    else
+    {
+      $(this).fadeIn();
+    }
+
+    i++;
+    imgarray.push($(this));
+    pic_height = ($(this)[0].naturalHeight);
+    pic_width = ($(this)[0].naturalWidth);
+
+    if ( (pic_height > 450)  || (pic_width > 740) )
+    {
+      // transform
+      if ( pic_width > pic_height )
+      {
+        factor = pic_width/pic_height;
+        final_width = 740;
+        while ( (final_width/factor) > 430 )
+        {
+          final_width = final_width - 1;
+        }
+        final_height = final_width/factor;
+        $(this).css('width',String(final_width) + 'px');
+        $(this).css('heigth', String(final_height) + 'px');
+      }
+      else
+      {
+        factor = pic_height/pic_width;
+        final_height = 450;
+        while ( (final_height/factor) > 700 )
+        {
+          final_height = final_height - 1;
+        }
+        final_width = final_height/factor;
+        $(this).css('heigth',String(final_height) + 'px');
+        $(this).css('width', String(final_width) + 'px');
+      }
+
+    }
+
+    // locate with padding-left and top, calculate center point
+    position_x = 366 - ($(this)[0].width)/2;
+    $(this).css('padding-left',String(position_x) + 'px');
+    position_y = 225 - ($(this)[0].height)/2;
+    $(this).css('padding-top',String(position_y) + 'px');
+  });
+
+  showing_pic = 0;
+
+  $('#gallery-counter').html(String(showing_pic + 1) + '/' + String(imgarray.length));
+
+  $('#left-arrow').click(function(){
+    if (showing_pic == imgarray.length-1)
+    {
+      $('#right-arrow').fadeIn('slow');
+    }
+    if (showing_pic == 1)
+    {
+      $('#left-arrow').fadeOut();
+    }
+    if (showing_pic > 0)
+    {
+      $('.picviewer:visible').fadeOut('slow');
+      showing_pic = showing_pic - 1;
+      // setTimeout(function(){
+        imgarray[showing_pic].fadeIn('slow');
+      // }, 600);
+    }
+    else
+    {
+      $('#left-arrow').fadeOut();
+    }
+    $('#gallery-counter').html(String(showing_pic + 1) + '/' + String(imgarray.length))
+  });
+
+  $('#right-arrow').click(function(){
+    if (showing_pic == 0)
+    {
+      $('#left-arrow').fadeIn('slow');
+    }
+    if (showing_pic == imgarray.length-2)
+    {
+      $('#right-arrow').fadeOut('slow');
+    }
+    if (showing_pic < imgarray.length-1)
+    {
+      $('.picviewer:visible').fadeOut('slow');
+      showing_pic = showing_pic + 1;
+      // setTimeout(function(){
+        imgarray[showing_pic].fadeIn('slow');
+      // }, 600);
+    }
+    else
+    {
+      $('#right-arrow').fadeOut();
+    }
+    $('#gallery-counter').html(String(showing_pic + 1) + '/' + String(imgarray.length))
+  });
+
+  $('.up-arrow:visible').click(function(){
+    $(this).parent().children().scrollTop($(this).parent().children().scrollTop() - 40);
+  });
+
+  $('.down-arrow:visible').click(function(){
+    $(this).parent().children().scrollTop($(this).parent().children().scrollTop() + 40);
+  });
+
+
+  //
+  // Audio code
+  //
+
+  // Load track urls in an array (manually)
+  // TO DO: check browser and load ogg for other browsers
+  audioarray = new Array();
+  item = new Array();
+  item[0] = 'resources/audio/chopin.wav';
+  item[1] = 'Chopin';
+  audioarray.push(item);
+  item = new Array();
+  // item[0] = 'resources/audio/goforit.mp3';
+  // item[1] = 'Tapiador - Go For It';
+  // audioarray.push(item);
+  // item = new Array();
+  // item[0] = 'resources/audio/krakow.mp3';
+  // item[1] = 'Tapiador - Krakow';
+  // audioarray.push(item);
+
+  // Select next song randomly
+  next_track = Math.floor((Math.random() * audioarray.length) + 1);
+  $("#audio-control").attr('src',audioarray[next_track-1][0]);
+  $("#audio-control").trigger('play');
+  $("#roll").html(':: Now playing :: ' + audioarray[next_track-1][1] + ' :: ');
+  $('#roll').bind('marquee', function() {
+    var ob = $(this);
+    var tw = ob.width();
+    var ww = ob.parent().width();
+    ob.css({ right: -tw });
+    ob.animate({ right: ww }, 12000, 'linear', function() {
+        ob.trigger('marquee');
+    });
+  }).trigger('marquee');
+
+  $("#audio-control").bind("ended", function(){
+    prev_track = next_track;
+    while (next_track == prev_track)
+    {
+      next_track = Math.floor((Math.random() * audioarray.length) + 1);
+    }
+    $("#audio-control").attr('src',audioarray[next_track-1][0]);
+    $("#audio-control").trigger('play');
+    $("#roll").html(':: Now playing :: ' + audioarray[next_track-1][1] + ' :: ');
+  });
+
+  $('#audio-control').click(function(){
+    audio_elem = document.getElementById('audio-control');
+    if (audio_elem.muted == false)
+    {
+      $('#track-test').fadeOut();
+    }
+  });
+
+});
---
+++ js/textlib.js
@@ -0,0 +1,266 @@
+// English content
+var english_link_about = "About";
+var english_link_calendar = "Calendar";
+var english_link_repertoire = "Repertoire";
+var english_link_multimedia = "Multimedia";
+var english_link_gallery = "Gallery";
+var english_link_contact = "Contact";
+
+var english_frontpage = "<p id='info'>The Duo Buitenkant-Reitz was founded in 2008. The combination" +
+  " of a broad repertoire, high-level playing together and their love for well-known and" +
+  " unsung compositions from the entire music literature resulted in many succesfull concerts.</p>";
+
+var english_about = "<p id='about'><u>About</u></p><br>" +
+  "<p id='about'>The Duo Buitenkant-Reitz was founded in 2008. The musicians got" +
+  " to know eachother one year before at the Jolimont-music course in Switzerland, playing" +
+  " together in a piano trio. After two summers full of music they decided to continue as" +
+  " a duet, and started to play several concert tours througout Switzerland and the Netherlands.</p><br>" +
+  "<p id='about'>After five years of working together, their repertoire already contains a" +
+  " major part of the well-known cello-piano literature, as well as a lot of unknown and" +
+  " unsung compositions. Every time the musicians enjoy practising new works, giving the" +
+  "audience an opportunity to get to know unknown pieces. With great precizeness they put" +
+  "together their programs. Especially the formula of a big famous composition together with" +
+  " a seldomly played one often turned out to be a big succes.</p><br>" +
+  "<p id='about'>The duo plays music from baroque till 20th century. One of their specialities" +
+  " are the sonatas by Ludwig van Beethoven that were part of a program already over 25 times." +
+  " They are planning to record all five sonatas in 2020, the great Beethoven anniversary jear." +
+  " Other beloved composers are Bach, Debussy and generally works from the romantic epoque.</p><br>";
+
+var english_calendar = "<p id='about'><u>Calendar</u></p><br>";
+
+var english_repertoire = "<p id='about'><u>Repertoire</u></p><br>" +
+  "<table><tr><td><p id='about'>Bach</p></td>"+
+    "<td><p id='about'>sonata no. 3 in g-minor, BWV 1029</p></td>" +
+  "</tr><tr></tr><tr></tr>" +
+  "<tr><td><p id='about'>Beethoven</p></td>" +
+    "<td><p id='about'>sonata no. 1 in F-major, op. 5/1</p>" +
+    "<p id='about'>sonata no. 2 in g-minor, op. 5/2</p>" +
+    "<p id='about'>sonata no. 3 in A-major, op. 69</p>" +
+    "<p id='about'>sonata no. 4 in C-major, op. 102/1</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Brahms</p></td>" +
+    "<td><p id='about'>sonata no. 1 in e-minor, op. 38</p>" +
+    "<p id='about'>sonata no. 2 in F-major, op. 99</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Chopin</p></td>" +
+    "<td><p id='about'>sonata in g-minor, op. 65</p>" +
+    "<p id='about'>Introduction et Grande Polonaise</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Debussy</p></td>" +
+    "<td><p id='about'>sonata in d-minor</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Fauré</p></td>" +
+    "<td><p id='about'>Élégie in c-minor, op. 24</p>" +
+    "<p id='about'>Romance in A-major, op. 69</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>C. Franck</p></td>" +
+    "<td><p id='about'>sonata in A-major</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Mendelssohn</p></td>" +
+    "<td><p id='about'>sonata no. 1 in B-major, op. 45</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Saint-Saëns</p></td>" +
+    "<td><p id='about'>The Swan</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Schubert</p></td>" +
+    "<td><p id='about'>sonata “Arpeggione” in a-minor, D 821</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Shostakovich</p></td>" +
+    "<td><p id='about'>sonata in d-minor, op. 40</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Ca. Schumann</p></td>" +
+    "<td><p id='about'>sonata no. 1 in g-minor, op. 59</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>R. Schumann</p></td>" +
+    "<td><p id='about'>Adagio and Allegro, op. 70</p>" +
+    "<p id='about'>Fantasy Pieces, op. 73</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>R. Strauss</p></td>" +
+    "<td><p id='about'>sonata in F-major, op. 6</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Zemlinksy</p></td>" +
+    "<td><p id='about'>sonata in a-minor</p></td>" +
+  "</tr></table>";
+
+// var english_gallery = "<p id='about'><u>Gallery</u></p><br>";
+
+var english_contact = "<p id='about'><u>Contact</u></p><br>";
+
+// German content
+var german_link_about = "Über uns";
+var german_link_calendar = "Kalender";
+var german_link_repertoire = "Repertoire";
+var german_link_multimedia = "Multimedia";
+var german_link_gallery = "Bilder";
+var german_link_contact = "Kontakt";
+
+var german_frontpage = "<p id='info'>Das Duo Buitenkant-Reitz wurde 2008 gegründet. Die Kombination" +
+  " eines breiten Repertoires, hochleistigen Zusammenspiels und Liebe für bekannte und" +
+  " unbekannte werke der gesamten Literatur führte schon in vielen erfolgreichen Konzerte.</p>"
+
+var german_about = "<p id='about'><u>Über uns</u></p><br>" +
+  "<p id='about'>Das Duo Buitenkant-Reitz wurde 2008 gegründet. Die Musiker lernten sich bereits ein Jahr" +
+    "früher am “Jolimont-Musiklager” in der Schweiz kennen, wo sie anfangs in der Triobesatzung spielten." +
+    "Nach Zwei Sommer voller Musik entschieden Sie als festes Duo weiter zu gehen, und fingen an regelmäßig " +
+    "Konzert-Tours zu spielen in der Schweiz und auch Holland.</p><br>" +
+  "<p id='about'>Schon nach 5 Jahren Zusammenarbeit umfasste ihr Repertoire einen grossen Teil der bekannten" +
+    "Cello-Klavier-Literatur, vor allem Sonaten, sowie ein Haufen seltsam gespielte und 'Ungesungene' Werke." +
+    "Immer wieder haben die beide Musiker daran grosses Vergnügen, neue Werke zu Proben und auf zu führen," +
+    "und gleichzeitig das Publikum die Gelegenheit zu geben, unbekannte Werke kennen lernen zu lernen. Mit" +
+    "großer Sorgfaltigkeit werden die Programme zusammengestelt. Vor allem der Formel eines bekannten großen" +
+    "werkes zusammen mit ein selten aufgeführtes war schon oft ein großer Erfolg.</p><br>" +
+  "<p id='about'>Das Duo spielt Musik ab Barok bis im 20. Jahrhundert. Eine besondere Spezialität der beiden" +
+    "sind die Sonaten von Ludwig van Beethoven, die mitterweile schon über 25 Mal auf einem Konzertprogram" +
+    "standen. Sie haben vor im 2020, das Große Beethoven-Jubiläum, alle 5. sonaten, sowie die Variationswerke " +
+    "aufzunehmen. Gerne spielen sie sonst Bach, Debussy und große romantische Werke.</p><br>";
+
+var german_calendar = "<p id='about'><u>Kalender</u></p><br>";
+
+var german_repertoire = "<p id='about'><u>Repertoire</u></p><br>" +
+  "<table><tr><td><p id='about'>Bach</p></td>"+
+    "<td><p id='about'>sonate nr. 3 in g, BWV 1029</p></td>" +
+  "</tr><tr></tr><tr></tr>" +
+  "<tr><td><p id='about'>Beethoven</p></td>" +
+    "<td><p id='about'>sonate nr. 1 in F, op. 5/1</p>" +
+    "<p id='about'>sonate nr. 2 in g, op. 5/2</p>" +
+    "<p id='about'>sonate nr. 3 in A, op. 69</p>" +
+    "<p id='about'>sonate nr. 4 in C, op. 102/1</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Brahms</p></td>" +
+    "<td><p id='about'>sonate nr. 1 in e, op. 38</p>" +
+    "<p id='about'>sonate nr. 2 in F, op. 99</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Chopin</p></td>" +
+    "<td><p id='about'>sonate in g, op. 65</p>" +
+    "<p id='about'>Introduction et Grande Polonaise</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Debussy</p></td>" +
+    "<td><p id='about'>sonate in d</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Fauré</p></td>" +
+    "<td><p id='about'>Élégie in c, op. 24</p>" +
+    "<p id='about'>Romance in A, op. 69</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>C. Franck</p></td>" +
+    "<td><p id='about'>sonate in A</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Mendelssohn</p></td>" +
+    "<td><p id='about'>sonate nr. 1 in B, op. 45</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Saint-Saëns</p></td>" +
+    "<td><p id='about'>der Schwan</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Schubert</p></td>" +
+    "<td><p id='about'>sonate “Arpeggione” in a, D 821</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Shostakowitsch</p></td>" +
+    "<td><p id='about'>sonate in d, op. 40</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Ca. Schumann</p></td>" +
+    "<td><p id='about'>sonate nr. 1 in g, op. 59</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>R. Schumann</p></td>" +
+    "<td><p id='about'>Adagio und Allegro, op. 70</p>" +
+    "<p id='about'>Fantasiestücke, op. 73</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>R. Strauss</p></td>" +
+    "<td><p id='about'>sonate in F, op. 6</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Zemlinksy</p></td>" +
+    "<td><p id='about'>sonate in a</p></td>" +
+  "</tr></table>";
+
+// var german_gallery =
+//   "<div id='thumbnails'>" +
+//     "<ul class='clearfix'>" +
+//     "<li><a href='resources/images/buitenkant-reitz.jpg' title='Buite'><img src='resources/images/facebook_logo.png' alt='facebook'></a></li>" +
+//     "</ul></div>";
+
+var german_contact = "<p id='about'><u>Kontact</u></p><br>";
+
+// Polish content
+var polish_link_about = "O nas";
+var polish_link_calendar = "Kalendarz";
+var polish_link_repertoire = "Repertuar";
+var polish_link_multimedia = "Multimedia";
+var polish_link_gallery = "Galeria";
+var polish_link_contact = "Kontakt";
+
+var polish_frontpage = "<p id='info'>Duo Buitenkant-Reitz powstał w roku 2008. Kombinacja szerokiego repertuaru, współgrania" +
+  " na wysokim poziomie oraz ich miłość dla słynnych i" +
+  " nieznanych utworów z całej literatury muzycznej wynikała już w dużo udanych koncertów.</p>";
+
+var polish_about = "<p id='about'><u>O nas</u></p><br>" +
+  "<p id='about'>Duo Buitenkant-Reitz zostało ustanawiony w roku 2008. Muzycy się poznali już" +
+  " rok wcześniej na kursie muzycznej 'Jolimont' w Szwajcarii, grając tam razem w trio" +
+  " fortepianowym. Po dwóch letnich czasach pełno muzyki, zdecydowali kontinuować jako duet i" +
+  " zaczęli grać liczne kolei koncertów w Szwajcarii oraz w Holandii.</p><br>" +
+  "<p id='about'>Po pięciu lat współpracy ich repertuar już zawiera wielką część znanej" +
+  " literatury na wiolonczeli i fortepianie także dużo nieznanych i rzadko granych utworów." +
+  " Zawsze z wielką radościa ćwiczą nowe utwory, dając publikum okazję żeby poznać nieznane" +
+  " kawałki. Zwłaszcza formuła jednej sławnej kompozycji razem z mało granym dziełem okazało się" +
+  " często jako wielki sukses.</p><br>" +
+  "<p id='about'>Duo gra dzieła od czasu barokowego do 20ego wieku. Jeden z ich specjalności są" +
+  " sonaty Ludwiga van Beethovena, które już się znalazły na programie ponad 25 razy. Oni mają" +
+  " zamiar nagrywać wszystkie sonaty w roku Beethovena w 2020. Oprócz tego grają z chęcią muzykę" +
+  " Bacha, Debussy' ego i z epoku romantycznego.</p><br>";
+
+var polish_calendar = "<p id='about'><u>Kalendarz</u></p><br>";
+
+var polish_repertoire = "<p id='about'><u>Repertuar</u></p><br>" +
+  "<table><tr><td><p id='about'>Bach</p></td>"+
+    "<td><p id='about'>sonata no. 3 g-moll, BWV 1029</p></td>" +
+  "</tr><tr></tr><tr></tr>" +
+  "<tr><td><p id='about'>Beethoven</p></td>" +
+    "<td><p id='about'>sonata no. 1 F-dur, op. 5/1</p>" +
+    "<p id='about'>sonata no. 2 g-moll, op. 5/2</p>" +
+    "<p id='about'>sonata no. 3 A-dur, op. 69</p>" +
+    "<p id='about'>sonata no. 4 C-dur, op. 102/1</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Brahms</p></td>" +
+    "<td><p id='about'>sonata no. 1 e-moll, op. 38</p>" +
+    "<p id='about'>sonata no. 2 F-dur, op. 99</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Chopin</p></td>" +
+    "<td><p id='about'>sonata g-moll, op. 65</p>" +
+    "<p id='about'>Introduction et Grande Polonaise</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Debussy</p></td>" +
+    "<td><p id='about'>sonata d-moll</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Fauré</p></td>" +
+    "<td><p id='about'>Élégie c-moll, op. 24</p>" +
+    "<p id='about'>Romance A-dur, op. 69</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>C. Franck</p></td>" +
+    "<td><p id='about'>sonata A-dur</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Mendelssohn</p></td>" +
+    "<td><p id='about'>sonata no. 1 B-dur, op. 45</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Saint-Saëns</p></td>" +
+    "<td><p id='about'>Łabędź</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Schubert</p></td>" +
+    "<td><p id='about'>sonata “Arpeggione” a-moll, D 821</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Szostakowicz</p></td>" +
+    "<td><p id='about'>sonata d-moll, op. 40</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Ca. Schumann</p></td>" +
+    "<td><p id='about'>sonata no. 1 g-moll, op. 59</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>R. Schumann</p></td>" +
+    "<td><p id='about'>Adagio i Allegro, op. 70</p>" +
+    "<p id='about'>Fantasiestücke, op. 73</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>R. Strauss</p></td>" +
+    "<td><p id='about'>sonata F-dur, op. 6</p></td>" +
+  "</tr><tr></tr><tr></tr><tr>" +
+  "<td><p id='about'>Zemlinksy</p></td>" +
+    "<td><p id='about'>sonata a-moll</p></td>" +
+  "</tr></table>";
+
+  // var polish_gallery = "<p id='about'><u>Galeria</u></p><br>";
+
+  var polish_contact = "<p id='about'><u>Kontact</u></p><br>";
