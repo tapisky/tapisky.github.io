316c2c1a7b6a0fbda7881ba0f63c060a0257657e
tapisky <jctapiador@gmail.com>
2014-07-09 19:42:07 +0200

    Fixed Contact info

--- js/textlib.js
+++ js/textlib.js
@@ -83,7 +83,7 @@ var english_repertoire = "<p id='about'><u>Repertoire</u></p><br>" +
 
 // var english_gallery = "<p id='about'><u>Gallery</u></p><br>";
 
-var english_contact = "Address<br>";
+var english_contact = "<p id='about'>Address</p>";
 
 // German content
 var german_link_about = "Über uns";
