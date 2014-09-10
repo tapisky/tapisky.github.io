77f21b3092f9a8eec45ebfc9cdf85db7590a3e0f
tapisky <jctapiador@gmail.com>
2014-09-10 13:14:10 +0200

    Updated calendar as of 10.09.2014

--- js/textlib.js
+++ js/textlib.js
@@ -25,7 +25,10 @@ var english_about = "<p id='about'><u>About</u></p><br>" +
   "They plan to record all five sonatas in 2020, the year marking the anniversary of Beethoven's birth. " +
   "The musicians' other favourites are works by Bach, Debussy, as well as these from the Romantic period.</p><br>";
 
-var english_calendar = "<p id='about'><u>Calendar</u></p><br>";
+var english_calendar = "<p id='about'><u>Calendar</u></p><br>" +
+  "<p><b>29 september/wrzesień --- KRAKÓW --- 19:30</b></p>" +
+  "<p>Akademia Muzyczna, sala koncertowa</p>" +
+  "<p>ul. św. Tomasza 43</p><br><br>";
 
 var english_repertoire = "<p id='about'><u>Repertoire</u></p><br>" +
   "<table><tr><td><p id='about'>Bach</p></td>"+
@@ -113,7 +116,9 @@ var german_about = "<p id='about'><u>Über uns</u></p><br>" +
   'im Jahr 2020, dem grossen Jubiläumsjahr Beethovens, Aufnahmen aller fünf Sonaten sowie der Variationswerke zu veröffentlichen. ' +
   'Ansonsten gehören etwa Bach, Debussy und viele grosse Werke der Romantik zu ihrem Repertoire.</p><br>';
 
-var german_calendar = "<p id='about'><u>Kalender</u></p><br>";
+var german_calendar = "<p><b>29 september/wrzesień --- KRAKÓW --- 19:30</b></p>" +
+  "<p>Akademia Muzyczna, sala koncertowa</p>" +
+  "<p>ul. św. Tomasza 43</p><br><br>";
 
 var german_repertoire = "<p id='about'><u>Repertoire</u></p><br>" +
   "<table><tr><td><p id='about'>Bach</p></td>"+
@@ -203,7 +208,9 @@ var polish_about = "<p id='about'><u>O nas</u></p><br>" +
   "Muzycy zamierzają nagrać wszystkie pięć sonat w roku 2020, będącym rokiem beethovenowskim z racji okrągłej rocznicy " +
   "narodzin kompozytora. Inne ulubione kompozycje duetu, to utwory Bacha, Debussy'ego oraz muzyczna literatura romantyzmu.</p><br>";
 
-var polish_calendar = "<p id='about'><u>Kalendarz</u></p><br>";
+var polish_calendar = "<p><b>29 september/wrzesień --- KRAKÓW --- 19:30</b></p>" +
+  "<p>Akademia Muzyczna, sala koncertowa</p>" +
+  "<p>ul. św. Tomasza 43</p><br><br>";
 
 var polish_repertoire = "<p id='about'><u>Repertuar</u></p><br>" +
   "<table><tr><td><p id='about'>Bach</p></td>"+
